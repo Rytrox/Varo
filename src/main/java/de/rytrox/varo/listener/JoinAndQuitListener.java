26b17eb9c558682dfff98308af67990d610ef52c
Sether701 <sether017@gmail.com>
2021-09-19 11:54:11 +0200

    created listener and react on events

---
+++ src/main/java/de/rytrox/varo/listener/JoinAndQuitListener.java
@@ -0,0 +1,22 @@
+package de.rytrox.varo.listener;
+
+import de.rytrox.varo.Varo;
+import de.rytrox.varo.utils.DiscordService;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.event.player.PlayerQuitEvent;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class JoinAndQuitListener implements Listener {
+
+    @EventHandler
+    public void onJoin(PlayerJoinEvent event) {
+        JavaPlugin.getPlugin(Varo.class).getDiscordWebhook().writeMessage(event.getJoinMessage(), DiscordService.DiscordColor.BLUE);
+    }
+
+    @EventHandler
+    public void onLeave(PlayerQuitEvent event) {
+        JavaPlugin.getPlugin(Varo.class).getDiscordWebhook().writeMessage(event.getQuitMessage(), DiscordService.DiscordColor.RED);
+    }
+}
