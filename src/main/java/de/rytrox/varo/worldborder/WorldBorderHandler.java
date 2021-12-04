cc416ab18441dbb7fb39e697786804d342478047
Sether701 <sether017@gmail.com>
2021-12-04 10:39:08 +0100

    VARO-17: when player spawns outside the worldborder, tp to center and leak coordinates

--- src/main/java/de/rytrox/varo/worldborder/WorldBorderHandler.java
+++ src/main/java/de/rytrox/varo/worldborder/WorldBorderHandler.java
@@ -2,15 +2,18 @@ package de.rytrox.varo.worldborder;
 
 import de.rytrox.varo.Varo;
 import de.rytrox.varo.database.repository.TeamMemberRepository;
+import de.rytrox.varo.discord.MessageService;
 import de.rytrox.varo.gamestate.GameStateHandler;
 import de.rytrox.varo.resurrection.PlayerResurrectionEvent;
 import org.bukkit.Bukkit;
 import org.bukkit.Location;
 import org.bukkit.World;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.entity.EntityDamageEvent;
 import org.bukkit.event.entity.PlayerDeathEvent;
-import org.bukkit.scheduler.BukkitTask;
+import org.bukkit.event.player.PlayerJoinEvent;
 import org.jetbrains.annotations.NotNull;
 
 public class WorldBorderHandler implements Listener {
@@ -30,7 +33,7 @@ public class WorldBorderHandler implements Listener {
         Bukkit.getPluginManager().registerEvents(this, main);
 
         this.intialSize = main.getConfig().getInt("worldborder.intialSize");
-        this.currentSize = this.intialSize;
+        this.currentSize = this.intialSize; // TODO should be one of those single-value that should be saved in a json later (VARO-28)
 
         TeamMemberRepository teamMemberRepository = new TeamMemberRepository(main.getDB());
         this.totalPlayers = teamMemberRepository.getTotalPlayerAmount();
@@ -43,6 +46,7 @@ public class WorldBorderHandler implements Listener {
 
         world.getWorldBorder().setCenter(center);
         world.getWorldBorder().setSize(intialSize);
+        world.getWorldBorder().setDamageAmount(0.1D);
 
     }
 
@@ -74,5 +78,46 @@ public class WorldBorderHandler implements Listener {
         this.alivePlayers++;
     }
 
+    @EventHandler
+    public void onJoin(PlayerJoinEvent event) {
+        this.checkSpawnPosition(event.getPlayer());
+    }
+
+    /**
+     * checks if a given player has spawned within the worldborder and teleports them to the center if they are not
+     * @param player Player you want to check
+     */
+    private void checkSpawnPosition(Player player) {
+        if(!isInsideWorldBorder(player)) {
+            player.teleport(center);
+            MessageService.getInstance().leakPlayerCoordinates(player, MessageService.CoordinateLeakReason.SPAWN_OUTSIDE_BORDER);
+        }
+    }
+
+    /**
+     * Checks if a given player is inside the worldborder
+     * @param player The player you want to check
+     * @return result of check (also true if the player is in another world)
+     */
+    private boolean isInsideWorldBorder(Player player) {
+        Location playerLocation = player.getLocation();
+
+        // check if player is in the same world as the worldborder
+        if(!(playerLocation.getWorld().getName().equals(center.getWorld().getName()))) {
+            return true;
+        }
+
+        double worldBorderSize = center.getWorld().getWorldBorder().getSize();
+
+        double maxX = center.getX() + worldBorderSize/2;
+        double maxY = center.getY() + worldBorderSize/2;
+        double minX = center.getX() + worldBorderSize/2;
+        double minY = center.getY() + worldBorderSize/2;
+
+        double playerX = playerLocation.getX();
+        double playerY = playerLocation.getY();
+
+        return playerX <= maxX && playerX >= minX && playerY <= maxY && playerY >= minY;
+    }
 
 }
