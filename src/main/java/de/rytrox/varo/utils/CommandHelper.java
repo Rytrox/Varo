b8be1ce168357ae9920598f49beaddb639fa19f1
Sether701 <sether017@gmail.com>
2021-12-17 18:39:27 +0100

    VARO-43: added methods to command helper for directly printing the message

--- src/main/java/de/rytrox/varo/utils/CommandHelper.java
+++ src/main/java/de/rytrox/varo/utils/CommandHelper.java
@@ -1,11 +1,31 @@
 package de.rytrox.varo.utils;
 
 import org.bukkit.ChatColor;
+import org.bukkit.command.CommandSender;
 
 /**
- * An Util-class for a unified command design
+ * An Util-class for a unified command helping design
  */
 public class CommandHelper {
+
+    /**
+     * Takes the command input and prints a corresponding header string
+     * @param commandSender The command sender that needs help
+     * @param command The command you want to create the header for
+     */
+    public static void sendCommandHeader(String command, CommandSender commandSender) {
+        commandSender.sendMessage(formatCommandHeader(command));
+    }
+
+    /**
+     * Takes the command and its explanation and prints a corresponding help string
+     * @param command The command you want to explain
+     * @param explanation the explanation for the command
+     * @param commandSender The command sender that needs help
+     */
+    public static void sendCommandExplanation(String command, String explanation, CommandSender commandSender) {
+        commandSender.sendMessage(formatCommandExplanation(command, explanation));
+    }
 
     /**
      * Takes the command input and creates a corresponding header string
