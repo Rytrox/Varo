d30d699d906af17c996bcd4029d1477f5ae19138
Timeout <taubmann870@gmail.com>
2021-12-26 15:43:23 +0100

    VARO-15: Finish PlayerTime limitation

--- src/main/java/de/rytrox/varo/game/PlayerTimeService.java
+++ src/main/java/de/rytrox/varo/game/PlayerTimeService.java
@@ -13,38 +13,40 @@ import de.rytrox.varo.teams.events.TeamMemberSpawnEvent;
 import net.minecraft.server.v1_8_R3.IChatBaseComponent;
 import net.minecraft.server.v1_8_R3.PacketPlayOutChat;
 
+import org.apache.commons.lang.time.DurationFormatUtils;
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
-import org.bukkit.OfflinePlayer;
 import org.bukkit.craftbukkit.v1_8_R3.entity.CraftPlayer;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.scheduler.BukkitTask;
 import org.jetbrains.annotations.NotNull;
 
+import java.time.Duration;
 import java.time.LocalDate;
 import java.time.LocalTime;
 import java.time.format.DateTimeFormatter;
-import java.util.HashMap;
+import java.util.LinkedHashMap;
 import java.util.Map;
+import java.util.UUID;
 
 public class PlayerTimeService implements Listener {
 
     private final PlayerTimeStatisticRepository playerTimeStatisticRepository;
 
-    private final Map<OfflinePlayer, LocalTime> playerDayTime = new HashMap<>();
+    private final Map<UUID, Duration> playerDayTime = new LinkedHashMap<>();
 
     private final Varo main;
     private BukkitTask playerTimeTask;
 
     private final LocalTime dayLength;
-    private final int minAllowedDays;
+    private final int allowedSkipDays;
     private final int maxAllowedDays;
 
     public PlayerTimeService(@NotNull Varo main) {
         this.main = main;
 
-        this.minAllowedDays = main.getConfig().getInt("game.playertime.min-allowed-days", -3);
+        this.allowedSkipDays = main.getConfig().getInt("game.playertime.allowed-skip-days", 2);
         this.maxAllowedDays = main.getConfig().getInt("game.playertime.max-allowed-days", 3);
         this.dayLength = LocalTime.parse(main.getConfig().getString("game.playertime.day-length", "00:15"));
         this.playerTimeStatisticRepository = new PlayerTimeStatisticRepository(main.getDB());
@@ -52,29 +54,37 @@ public class PlayerTimeService implements Listener {
 
     @EventHandler
     public void onPlayerConnect(TeamMemberSpawnEvent event) {
+        // Do nothing when game has not been started yet
+        if(GameStateHandler.getInstance().getCurrentGameState() != GameStateHandler.GameState.MAIN)
+            return;
+
         // check if the player is not reconnected
-        if(!playerDayTime.containsKey(event.getPlayer())) {
+        if(!playerDayTime.containsKey(event.getPlayer().getUniqueId())) {
             // read his stats from database
             PlayerTimeStatistic statistic = event.getMember().getPlayerTimeStatistic();
 
             // check if his block is still valid
             if(statistic.isBlocked()) {
-                if(statistic.getAvailableDays() < 1) {
-                    event.getPlayer().kickPlayer(ChatColor.translateAlternateColorCodes('&',
+                if(statistic.getAvailableDays() < 0) {
+                    event.setCancelled(true);
+                    event.setCancelMessage(ChatColor.translateAlternateColorCodes('&',
                             "&8[&6Varo&8] &cDeine Sperre ist noch nicht aufgehoben. \n Versuche es am " +
                                     LocalDate.now().minusDays(statistic.getAvailableDays() - 1).format(DateTimeFormatter.ofPattern("dd.MM.yyyy")) + " erneut."));
+
                     return;
                 } else {
                     // Unblock player
                     statistic.setBlocked(false);
                 }
             } else {
-                statistic.setBlocked(this.minAllowedDays == statistic.getAvailableDays());
+                statistic.setBlocked(statistic.getAvailableDays() < -this.allowedSkipDays);
                 if(statistic.isBlocked()) {
-                    event.getPlayer().kickPlayer(ChatColor.translateAlternateColorCodes('&',
+                    event.setCancelled(true);
+                    event.setCancelMessage(ChatColor.translateAlternateColorCodes('&',
                             "&8[&6Varo&8] &cDu wurdest durch Überziehen deiner Spielzeit bis zum " +
-                                    LocalDate.now().minusDays(statistic.getAvailableDays() - 1).format(DateTimeFormatter.ofPattern("dd.MM.yyyy")) +
+                                    LocalDate.now().minusDays(statistic.getAvailableDays()).format(DateTimeFormatter.ofPattern("dd.MM.yyyy")) +
                                     " gesperrt. \n \n Bitte versuche es dann erneut!"));
+                    main.getDB().update(statistic);
                     return;
                 }
             }
@@ -82,7 +92,12 @@ public class PlayerTimeService implements Listener {
             // insert into local cache
             statistic.decreaseDays();
             main.getDB().update(statistic);
-            this.playerDayTime.put(event.getPlayer(), this.dayLength);
+
+            this.playerDayTime.put(event.getPlayer().getUniqueId(), Duration.between(LocalTime.now(), LocalTime.now()
+                    .plusHours(this.dayLength.getHour())
+                    .plusMinutes(this.dayLength.getMinute())
+                    .plusSeconds(this.dayLength.getSecond()))
+            );
         }
     }
 
@@ -98,23 +113,40 @@ public class PlayerTimeService implements Listener {
     public void onGameStateChange(GamestateChangeEvent event) {
         // start when next gamestate is ingame
         if(event.getNext() == GameStateHandler.GameState.MAIN) {
+            // Add all online players to the list
+            Bukkit.getOnlinePlayers().forEach(player -> this.playerDayTime.put(player.getUniqueId(), Duration.between(LocalTime.now(), LocalTime.now()
+                    .plusHours(this.dayLength.getHour())
+                    .plusMinutes(this.dayLength.getMinute())
+                    .plusSeconds(this.dayLength.getSecond()))
+            ));
+
             playerTimeTask = Bukkit.getScheduler().runTaskTimerAsynchronously(main, () -> {
                 // send time of every player to every Player in action bar
                 playerDayTime.entrySet()
                         .stream()
-                        .filter((entry) -> entry.getKey().isOnline())
+                        .filter((entry) -> Bukkit.getPlayer(entry.getKey()) != null)
                         .forEach((entry) -> {
-                            LocalTime newTime = entry.getValue().minusSeconds(1);
+                            Duration newTime = entry.getValue().minusSeconds(1);
 
-                            playerDayTime.put(entry.getKey(), newTime);
+                            if(!newTime.equals(Duration.ZERO)) {
+                                playerDayTime.put(entry.getKey(), newTime);
 
-                            // send time to action bar
-                            JsonObject message = new JsonObject();
-                            message.addProperty("text", ChatColor.translateAlternateColorCodes('&', "&5" +
-                                    newTime.format(DateTimeFormatter.ofPattern("hh:mm"))));
+                                // send time to action bar
+                                JsonObject message = new JsonObject();
+                                message.addProperty("text", ChatColor.translateAlternateColorCodes('&', "&5Verbleibende Spielzeit: " +
+                                        DurationFormatUtils.formatDuration(newTime.toMillis(), "mm:ss")));
 
-                            PacketPlayOutChat chatPacket = new PacketPlayOutChat(IChatBaseComponent.ChatSerializer.a(message.toString()), (byte) 2);
-                            ((CraftPlayer)entry.getKey().getPlayer()).getHandle().playerConnection.sendPacket(chatPacket);
+                                PacketPlayOutChat chatPacket = new PacketPlayOutChat(IChatBaseComponent.ChatSerializer.a(message.toString()), (byte) 2);
+                                ((CraftPlayer)Bukkit.getPlayer(entry.getKey())).getHandle().playerConnection.sendPacket(chatPacket);
+                            } else {
+                                playerDayTime.remove(entry.getKey());
+
+                                Bukkit.getScheduler().runTask(main, () ->
+                                    Bukkit.getPlayer(entry.getKey()).kickPlayer(ChatColor.translateAlternateColorCodes('&',
+                                            "&8[&6Varo&8] &cDein Spieltag ist vorbei. Morgen geht es weiter!")
+                                    )
+                                );
+                            }
                         });
             }, 0, 20L);
         } else if(this.playerTimeTask != null){
--- src/main/java/de/rytrox/varo/teams/events/TeamMemberSpawnEvent.java
+++ src/main/java/de/rytrox/varo/teams/events/TeamMemberSpawnEvent.java
@@ -3,17 +3,22 @@ package de.rytrox.varo.teams.events;
 import de.rytrox.varo.database.entity.TeamMember;
 
 import org.bukkit.entity.Player;
+import org.bukkit.event.Cancellable;
 import org.bukkit.event.HandlerList;
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
 /**
  * Event that will be triggered when a TeamMember joins the game
  *
  * @author Timeout
  */
-public class TeamMemberSpawnEvent extends TeamMemberEvent {
+public class TeamMemberSpawnEvent extends TeamMemberEvent implements Cancellable {
 
     private static final HandlerList HANDLER_LIST = new HandlerList();
+
+    private boolean cancel;
+    private String cancelMessage;
 
     public TeamMemberSpawnEvent(@NotNull Player player, @NotNull TeamMember member) {
         super(player, member);
@@ -26,5 +31,24 @@ public class TeamMemberSpawnEvent extends TeamMemberEvent {
     @Override
     public HandlerList getHandlers() {
         return getHandlerList();
+    }
+
+    @Override
+    public boolean isCancelled() {
+        return cancel;
+    }
+
+    @Override
+    public void setCancelled(boolean b) {
+        this.cancel = b;
+    }
+
+    @Nullable
+    public String getCancelMessage() {
+        return cancelMessage;
+    }
+
+    public void setCancelMessage(@Nullable String cancelMessage) {
+        this.cancelMessage = cancelMessage;
     }
 }
