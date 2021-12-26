47c0235d751397204acbd6df7b98846fff9f71da
Sether701 <sether017@gmail.com>
2021-12-26 14:20:57 +0100

    VARO-37: modify join and quit messages

--- src/main/java/de/rytrox/varo/message/MessageListener.java
+++ src/main/java/de/rytrox/varo/message/MessageListener.java
@@ -1,6 +1,9 @@
 package de.rytrox.varo.message;
 
 import de.rytrox.varo.Varo;
+import de.rytrox.varo.moderation.ModeratorManager;
+import de.rytrox.varo.scoreboard.Tablist;
+import net.md_5.bungee.api.ChatColor;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
@@ -8,6 +11,8 @@ import org.bukkit.event.player.PlayerQuitEvent;
 
 public class MessageListener implements Listener {
 
+    private static final String JOIN_MESSAGE = "%s%s &ahat den Server betreten";
+    private static final String QUIT_MESSAGE = "%s%s &chat den Server verlassen";
     private final Varo main;
 
     public MessageListener(Varo main) {
@@ -16,11 +21,23 @@ public class MessageListener implements Listener {
 
     @EventHandler
     public void onJoin(PlayerJoinEvent event) {
+
+        event.setJoinMessage(ChatColor.translateAlternateColorCodes('&',
+                String.format(JOIN_MESSAGE,
+                Tablist.getInstance().getPrefix(event.getPlayer()),
+                event.getPlayer().getName())));
+
         MessageService.getInstance().writeMessage(event.getJoinMessage(), MessageService.DiscordColor.BLUE);
     }
 
     @EventHandler
     public void onLeave(PlayerQuitEvent event) {
+
+        event.setQuitMessage(ChatColor.translateAlternateColorCodes('&',
+                String.format(QUIT_MESSAGE,
+                        Tablist.getInstance().getPrefix(event.getPlayer()),
+                        event.getPlayer().getName())));
+
         MessageService.getInstance().writeMessage(event.getQuitMessage(), MessageService.DiscordColor.RED);
     }
 }
