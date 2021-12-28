d62eca97b449f3d76044176a620e3121ac668a79
Timeout <taubmann870@gmail.com>
2021-12-28 20:29:12 +0100

    VARO-54: Teleport all Members to their Spawnpoint in GamePhase PRE_GAME

--- src/main/java/de/rytrox/varo/game/GameService.java
+++ src/main/java/de/rytrox/varo/game/GameService.java
@@ -1,6 +1,7 @@
 package de.rytrox.varo.game;
 
 import de.rytrox.varo.Varo;
+import de.rytrox.varo.database.repository.TeamMemberRepository;
 import de.rytrox.varo.gamestate.GameStateHandler;
 import de.rytrox.varo.gamestate.events.GamestateChangeEvent;
 import de.rytrox.varo.teams.events.TeamMemberSpawnEvent;
@@ -12,6 +13,9 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.jetbrains.annotations.NotNull;
 
+import java.util.Objects;
+import java.util.Optional;
+
 /**
  * Service for managing Ingame-Stuff
  */
@@ -19,8 +23,12 @@ public class GameService implements Listener {
 
     private final Varo main;
 
+    private final TeamMemberRepository teamMemberRepository;
+
     public GameService(@NotNull Varo main) {
         this.main = main;
+
+        this.teamMemberRepository = new TeamMemberRepository(main.getDB());
 
         Bukkit.getPluginManager().registerEvents(new GameTimeService(main), main);
         Bukkit.getPluginManager().registerEvents(new PlayerDeathListener(main), main);
@@ -38,6 +46,22 @@ public class GameService implements Listener {
                     .stream()
                     .filter((player) -> !main.getModeratorManager().isModerator(player)) // ignore moderators!
                     .forEach(player -> player.setGameMode(GameMode.ADVENTURE));
+        }
+    }
+
+    @EventHandler
+    public void onTeleportSpawn(GamestateChangeEvent event) {
+        if(event.getNext() == GameStateHandler.GameState.PRE_GAME) {
+            Bukkit.getScheduler().runTaskAsynchronously(main, () -> {
+                teamMemberRepository.getOnlineMembers()
+                        .forEach((member) ->
+                            Optional.ofNullable(member.getSpawnPoint())
+                                .ifPresent((spawn) ->
+                                    Objects.requireNonNull(member.getPlayer())
+                                           .teleport(member.getSpawnPoint().getLocation())
+                                )
+                        );
+            });
         }
     }
 
